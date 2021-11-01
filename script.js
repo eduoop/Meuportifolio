b7d876df41f76464fde6382a1aac63e1039d728b
eduoop <dudugoiabal@hotmail.com>
2021-11-01 13:40:07 -0300

    :package: calculo de orçamento

--- script.js
+++ script.js
@@ -3,3 +3,30 @@ var hamburguer = document.querySelector(".hamburguer")
 hamburguer.addEventListener('click', () => {
     document.querySelector('.container').classList.toggle("show-menu")
 })
+
+document.querySelector('#qtde').addEventListener('change', atualizarPreço)
+document.querySelector('#js').addEventListener('change', atualizarPreço)
+document.querySelector('#layout-sim').addEventListener('change', atualizarPreço)
+document.querySelector('#layout-nao').addEventListener('change', atualizarPreço)
+document.querySelector('#prazo').addEventListener('change', () => {
+    const prazo = document.querySelector('#prazo').value
+    document.querySelector('label[for=prazo]').innerHTML = `Prazo: ${prazo} semanas`
+    atualizarPreço()``
+})
+
+function atualizarPreço() {
+    const quantidade = document.querySelector('#qtde').value
+    const temJS = document.querySelector('#js').checked
+    const incluilayout = document.querySelector('#layout-sim').checked
+    const prazo = document.querySelector('#prazo').value
+
+
+
+    let preco = quantidade * 100;
+    if (temJS) preco *= 1.1
+    if(incluilayout) preco += 500
+    let taxaurgencia = 1 - prazo*0.1
+    preco *= 1 + taxaurgencia
+
+    document.querySelector('#preco').innerHTML = `R$ ${preco.toFixed(2)}`
+}
